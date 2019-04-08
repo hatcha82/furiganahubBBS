51e660c6ed7d94b3c8c78a4e6d6569110870fca8
hatcha82 <hatcha82@gmail.com>
2019-04-08 05:51:26 +0000

    mail check done

--- config.php
+++ config.php
@@ -158,7 +158,10 @@ define('G5_MOBILE_AGENT',   'phone|samsung|lgtel|mobile|[^A]skt|nokia|blackberry
 // lib/mailer.lib.php 에서 사용
 define('G5_SMTP',      '127.0.0.1');
 define('G5_SMTP_PORT', '25');
-
+define('G5_SMTP_AUTH','true');
+define('G5_SMTP_USER_ID' , 'cbha@t2l.co.kr');
+define('G5_SMTP_PASSWORD', 'cbha7036!@');
+define('G5_SMTP_SSL_SECURE','none');
 
 /********************
     기타 상수
