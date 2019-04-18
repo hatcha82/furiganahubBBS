cf998f1c68228a9bd97ff73d0903c72a68b3a35a
Changbeom Ha <hatcha82@gmail.com>
2019-04-18 17:10:03 +0900

    news bot changed

--- scheduler/nodejs/furiganaNewsScheduler/bbs_newsbot.js
+++ scheduler/nodejs/furiganaNewsScheduler/bbs_newsbot.js
@@ -5,6 +5,7 @@ const KuromojiAnalyzer = require('kuroshiro-analyzer-kuromoji')
 const kuroshiro = new Kuroshiro()
 const Sequelize = require('sequelize')
 const config = require('./config/config')
+var debug = true
 var sequelize = new Sequelize(
   config.db.database,
   config.db.user,
@@ -24,9 +25,7 @@ var detailCrawler = new Crawler({
   
 });
 
-
-
- function listCrawlerCallBack(error, res, done) {
+function listCrawlerCallBack(error, res, done) {
   if(error){
       console.log(error)
   }else{
@@ -36,6 +35,8 @@ var detailCrawler = new Crawler({
       var newsLit = $('ul.ymuiList li');
       
       newsLit.each(async function(){
+        if(debug){
+          
           var title = $(this).find('.fsmt').text()
           var newsURL = $(this).find('.fsmt').find('a').attr('href')
           var newsImagURL =$(this).find('img').attr('src')  
@@ -63,22 +64,78 @@ var detailCrawler = new Crawler({
           var sql = `SELECT * FROM g5_write_furigana_news WHERE  wr_link1 = '${newsURL}' limit 1`
           var article = await sequelize.query(sql, { type: sequelize.QueryTypes.SELECT})    
             sequelize.close()
-          if(article.length == 1){
-            console.log(`${newsCompanyName} 새로운 뉴스가 없습니다.`)
 
-          }else{                        
+           if(article.length == 1){
+            console.log(`${newsCompanyName} 새로운 뉴스가 없습니다.`)
+          }else{
             console.log(`새로운 뉴스가 있습니다. 본문을 찾습니다.`)
             var crawlerparam = [{
               uri: newsURL,                        
               callback: detailCrawlerCallBack,
               param : param,
+              preRequest: function(options, done) {
+                  var ranTime = Math.floor((Math.random() * 10) + 1)
+                  console.log(`본문 접속 ${ranTime}Sec 후에 진행... : ${newsURL} `)
+                  setTimeout(function() {
+                    done();
+                  }, 1000)
+              }
             }] 
-            detailCrawler.queue(crawlerparam)  
+            detailCrawler.queue(crawlerparam)   
           }
+          debug = true
+        }
     })
   }
   done();
 }
+
+ 
+
+var insertsql = `
+INSERT INTO gnu5.g5_write_furigana_news
+SELECT  null                      wr_id, 
+        -1                        wr_num, 
+        ''                        wr_reply, 
+        0                         wr_parent, 
+        0                         wr_is_comment, 
+        0                         wr_comment, 
+        ''                        wr_comment_reply, 
+        ''                        ca_name, 
+        'html2'                   wr_option, 
+        :title                  wr_subject,         
+        :article                wr_content, 
+        :newsUrl                 wr_link1, 
+        ''                        wr_link2, 
+        0                         wr_link1_hit, 
+        0                         wr_link2_hit, 
+        0                         wr_hit, 
+        0                         wr_good, 
+        0                         wr_nogood, 
+        'admin'                   mb_id, 
+        '*08426AD968865464212B51178AB8555438F2DD95'                        wr_password, 
+        :newsPublisher            wr_name, 
+        'hatcha82@gmail.com'      wr_email, 
+        ''                        wr_homepage, 
+        :newsPublishedDate        wr_datetime, 
+        0                         wr_file, 
+        :newsPublishedDate        wr_last, 
+        '172.17.0.1'              wr_ip, 
+        ''                        wr_facebook_user, 
+        ''                        wr_twitter_user, 
+        :newsImageUrl             wr_1,  -- newsImageUrl
+        :newsPublisher            wr_2,  -- newsPublisher
+        :newsPubllisherImageUrl   wr_3,  -- newsPubllisherImageUrl
+        :newsPublishedDate        wr_4,  -- newsPublishedDate
+        'N'                       wr_5,  -- naverBlogUpload           
+        ''                        wr_6,  -- naverBlogRefNo               
+        :titleFurigana            wr_7, -- subject_furigana
+        ''                        wr_8, -- subject_translate
+        :furigana                 wr_9 , -- furigana
+        ''                        wr_10 -- translate    
+ 
+ `
+
 async function detailCrawlerCallBack(error, res, done){
   if(error){
     console.log(error);
@@ -86,72 +143,30 @@ async function detailCrawlerCallBack(error, res, done){
     var $ = res.$;
     var param = res.options.param;
     var article = $('.yjDirectSLinkTarget').text();
-    
+    console.log(`
+    News Title : ${param.title}
+    Article
+    ${article}
+    `)
+        var sequelize = new Sequelize(
+          config.db.database,
+          config.db.user,
+          config.db.password,
+          config.db.options
+        )
     param.article = article
     param.titleFurigana = await kuroshiro.convert(param.title,{mode: 'furigana', to: 'hiragana', romajiSystem: 'passport'})
     param.furigana = await kuroshiro.convert(article,{mode: 'furigana', to: 'hiragana', romajiSystem: 'passport'})
     param.createdUserId = 3
     param.updatedUserId = 3
     ;
-    console.log(`
-News Title : ${param.title}
-Article
-${article}
-`)
-    var sequelize = new Sequelize(
-      config.db.database,
-      config.db.user,
-      config.db.password,
-      config.db.options
-    )
+
     
-    var sql = `
-   INSERT INTO gnu5.g5_write_furigana_news
-   SELECT  null                      wr_id, 
-           -1                        wr_num, 
-           ''                        wr_reply, 
-           0                         wr_parent, 
-           0                         wr_is_comment, 
-           0                         wr_comment, 
-           ''                        wr_comment_reply, 
-           ''                        ca_name, 
-           'html2'                   wr_option, 
-           :title                  wr_subject,         
-           :article                wr_content, 
-           :newsUrl        wr_link1, 
-           ''                        wr_link2, 
-           0                         wr_link1_hit, 
-           0                         wr_link2_hit, 
-           0                         wr_hit, 
-           0                         wr_good, 
-           0                         wr_nogood, 
-           'admin'                   mb_id, 
-           '*08426AD968865464212B51178AB8555438F2DD95'                        wr_password, 
-           :newsPublisher            wr_name, 
-           'hatcha82@gmail.com'      wr_email, 
-           ''                        wr_homepage, 
-           :newsPublishedDate        wr_datetime, 
-           0                         wr_file, 
-           :newsPublishedDate        wr_last, 
-           '172.17.0.1'              wr_ip, 
-           ''                        wr_facebook_user, 
-           ''                        wr_twitter_user, 
-           :newsImageUrl             wr_1,  -- newsImageUrl
-           :newsPublisher            wr_2,  -- newsPublisher
-           :newsPubllisherImageUrl   wr_3,  -- newsPubllisherImageUrl
-           :newsPublishedDate        wr_4,  -- newsPublishedDate
-           'N'                       wr_5,  -- naverBlogUpload           
-           ''                        wr_6,  -- naverBlogRefNo               
-           :titleFurigana            wr_7, -- subject_furigana
-           ''                        wr_8, -- subject_translate
-           :furigana                 wr_9 , -- furigana
-           ''                        wr_10 -- translate    
-    
-    `
-    const newArticle = await sequelize.query(sql,{replacements: param});
+   
+    const newArticle = await sequelize.query(insertsql,{replacements: param});
     console.log(newArticle);
 
-    sql = `update g5_board set bo_count_write = bo_count_write + 1 where bo_table = 'furigana_news'`
+    var sql = `update g5_board set bo_count_write = bo_count_write + 1 where bo_table = 'furigana_news'`
     const result = await sequelize.query(sql);
     console.log(result);
     sequelize.close();
