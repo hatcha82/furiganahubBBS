9256963b47833a52c7fbc57445eeb709a0cbd932
Changbeom Ha <hatcha82@gmail.com>
2019-04-22 14:34:17 +0900

    동화 최신 리스트 썸네일 오류 수정

--- theme/furiganahub/skin/latest/basic/latest.skin.furigana_douwas.php
+++ theme/furiganahub/skin/latest/basic/latest.skin.furigana_douwas.php
@@ -11,18 +11,19 @@ add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 
     <?php for ($i=0; $i<count($list); $i++) {  ?>
         
         <li class="borderBottom" style="line-height:30px;">
-        <?php  echo "<img style='float:left;margin-right:10px;margin-bottom:5px;max-width:100px;' src='$thumbImage'/>"       ?>
+        <?php  $thumbImage = $list[$i]['wr_1']; echo "<img style='float:left;margin-right:10px;margin-bottom:5px;max-width:100px;' src='$thumbImage'/>"       ?>
             <?php
             $subject = $list[$i]['wr_7'];
             $subject_title =  $subject;            
             if ($list[$i]['icon_secret']) echo "<i class=\"fa fa-lock\" aria-hidden=\"true\"></i><span class=\"sound_only\">비밀글</span> ";
             if ($list[$i]['icon_new']) echo "<span class=\"new_icon\">N<span class=\"sound_only\">새글</span></span>";
             if ($list[$i]['icon_hot']) echo "<span class=\"hot_icon\">H<span class=\"sound_only\">인기글</span></span>";
-            $thumbImage = $list[$i]['wr_1'];
+            $translate =  $list[$i]['wr_8'] ;   
             $href = $list[$i]['href'];
             $subject_title = $subject_title;
+            $translate = ($translate === '' ? '번역중' :  $translate);
             $subject = "<span class='furigana' style='line-height:40px;'><strong>$subject</strong></span><br><span style='color:#888'>$translate</span>";                        
-            $translate =  $list[$i]['wr_8'] ;                        
+                                 
             echo "<a href='$href' title='$subject_title' >$subject</a>";            
             echo "<div style='clear:both'>";
                 
