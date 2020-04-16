486b26290305ce5dddc24e1a971f1b8d12aaf0ce
Changbeom Ha <hatcha82@gmail.com>
2020-04-16 17:25:08 +0900

    latest 삭제

--- theme/furiganahub/index.php
+++ theme/furiganahub/index.php
@@ -26,13 +26,15 @@ include_once(G5_THEME_PATH.'/head.php');
     <div style="text-align: center;margin-bottom: 20px;">
     <iframe width="728" height="90" allowtransparency="true" src="https://tab2.clickmon.co.kr/pop/wp_ad_728.php?PopAd=CM_M_1003067%7C%5E%7CCM_A_1054519%7C%5E%7CAdver_M_1046207&rt_ad_id_code=RTA_106089&mon_rf=REFERRER_URL" frameborder="0" scrolling="no"></iframe>
     </div>
+
+
 <div class="latest_wr">
     <!--  사진 최신글2 { -->
     <?php
     // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
     // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
     // 테마의 스킨을 사용하려면 theme/basic 과 같이 지정
-    echo latest('theme/pic_basic', 'gallery', 5, 23);
+    // echo latest('theme/pic_basic', 'gallery', 5, 23);
     ?>
     <!-- } 사진 최신글2 끝 -->
 </div>
@@ -61,7 +63,7 @@ include_once(G5_THEME_PATH.'/head.php');
         // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
         // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
         // 테마의 스킨을 사용하려면 theme/basic 과 같이 지정
-        echo latest('theme/basic', $row['bo_table'], 5, 45);
+       // echo latest('theme/basic', $row['bo_table'], 5, 45);
         ?>
     </div>
     <?php
